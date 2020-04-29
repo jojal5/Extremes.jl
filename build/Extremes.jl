47de307933445e53aa4b999ed1aba068609e8a31
Jonathan Jalbert <jonathan.jalbert@polymtl.ca>
2020-04-29 11:12:08 -0400

    Build documentation structure

---
+++ build/Extremes.jl
@@ -0,0 +1,15 @@
+module Extremes
+
+using Distributions, DataFrames, Dates
+using Optim, NLSolversBase
+using SpecialFunctions, LinearAlgebra
+
+import Distributions.GeneralizedExtremeValue
+
+include("functions.jl")
+include("mle_functions.jl")
+include("bayes_functions.jl")
+
+export getcluster, gevfit, gpdfit, gevfitbayes,gpdfitbayes
+
+end # module
