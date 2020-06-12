c4be9b4cf5aef35033690632ce5c118a5000bfc4
MarilouF <farmer.marilou@hotmail.fr>
2020-06-12 13:53:43 -0400

    Add TODOs for missing tests

--- test/structures/fittedeva/pwmeva_test.jl
+++ test/structures/fittedeva/pwmeva_test.jl
@@ -1,7 +1,25 @@
 @testset "pwmeva.jl" begin
+    @testset "quantile(fm, p)" begin
+        # TODO : test with p outside of [0, 1]
+
+        # TODO : test with known values
+
+    end
+
+    @testset "parametervar(fm, nboot)" begin
+        # TODO : test with nboot < 0
+
+        # TODO : test with known values
+
+    end
+
+    @testset "quantilevar(fm, level, nboot)" begin
+        # TODO : test with known values
+    end
+
     @testset "Base.show(io, obj)" begin
         # TODO : Test outputs correctly
-        
+
     end
 
 end
