3367266ed4f655823f8c3673d917a0985051d505
Jonathan Jalbert <jonathan.jalbert@polymtl.ca>
2020-06-11 20:42:50 -0400

    Add approximate covariance matrix for the pwm method by bootstrap

--- src/structures/fittedeva/pwmeva.jl
+++ src/structures/fittedeva/pwmeva.jl
@@ -6,6 +6,32 @@ struct pwmEVA{T<:EVA} <: fittedEVA
 end
 
 """
+    parametervar(fm::pwmEVA, nboot::Int=1000)
+
+Estimate the parameter estimates covariance matrix by bootstrap.
+"""
+function parametervar(fm::pwmEVA, nboot::Int=1000)
+
+    @assert nboot>0 "the number of bootstrap samples should be positive."
+
+    y = fm.model.data
+    n = length(y)
+
+    θ̂ = Array{Float64}(undef, nboot, length(fm.θ̂))
+
+    for i=1:nboot
+        y = rand(y, n)            # Generate a bootstrap sample
+        θ̂[i,:] = gevfitpwm(y).θ̂   # Compute the parameter estimates
+    end
+
+    V = cov(θ̂)                    # Compute the approximate covariance matrix
+
+    return V
+
+end
+
+
+"""
     Base.show(io::IO, obj::pwmEVA)
 
 Override of the show function for the objects of type pwmEVA.
