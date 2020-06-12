f92d3a1c3a2f5cea5ad134ed2d177c587958d60f
MarilouF <farmer.marilou@hotmail.fr>
2020-06-12 09:51:10 -0400

    Add root tests

--- test/utils_test.jl
+++ test/utils_test.jl
@@ -1,48 +1,66 @@
 @testset "utils.jl" begin
     @testset "getcluster(y, μ₁, μ₂)" begin
-        # TODO : Add tests with known values?
+        # TODO : Add tests with known values (J)
     end
 
     @testset "getcluster(df, μ₁, μ₂)" begin
-        # TODO : Test with coltype[1] not a date
+        # coltype[1] not a date throws
+        df = DataFrame(a = [1.0], b = [1.0])
+        @test_throws AssertionError Extremes.getcluster(df, 1.0)
 
-        # TODO : Test with coltype[2] not a real
+        # coltype[2] not a real throws
+        df = DataFrame(a = [Date(2020)], b = [Date(2020)])
+        @test_throws AssertionError Extremes.getcluster(df, 1.0)
 
-        # TODO : Add tests with known values?
+        # TODO : Add tests with known values (J)
 
     end
 
     @testset "slicematrix(A; dims)" begin
-        # TODO : Test with dims > 2
-
-        # slice a matrix of size 10 x 5
         m = 10
         n = 5
         A = rand(1:10, m, n)
 
-        B = Extremes.slicematrix(A)
-        C = Extremes.slicematrix(A, dims = 2)
+        # dims > 2 throws
+        @test_throws AssertionError Extremes.slicematrix(A, dims = 3)
 
-        Aᴮ = Extremes.unslicematrix(B)
-        Aᶜ = Extremes.unslicematrix(C, dims = 2)
+        # slice a matrix of size 10 x 5 (dim = 1)
+        B = Extremes.slicematrix(A)
 
         @test length(B) == n
+
+        # slice a matrix of size 10 x 5 (dim = 1)
+        C = Extremes.slicematrix(A, dims = 2)
+
         @test length(C) == m
-        @test A == Aᴮ
-        @test A == Aᶜ
 
     end
 
     @testset "unslicematrix(B; dims)" begin
-        # TODO : Test with dims > 2
+        m = 10
+        n = 5
+        A = rand(1:10, m, n)
+        B = Extremes.slicematrix(A)
 
-        # TODO : Test with known values
+        # dims > 2 throws
+        @test_throws AssertionError Extremes.unslicematrix(B, dims = 3)
+
+        # unslice a matrix of size 10 x 5 (dim = 1)
+        Aᴮ = Extremes.unslicematrix(B)
+
+        @test A == Aᴮ
+
+        # unslice a matrix of size 10 x 5 (dim = 1)
+        C = Extremes.slicematrix(A, dims = 2)
+        Aᶜ = Extremes.unslicematrix(C, dims = 2)
+
+        @test A == Aᶜ
 
     end
 
     @testset "buildExplanatoryVariables(df, ids)" begin
         # TODO : Test with known values
-        
+
     end
 
 end
