9fd87fcd8ef525225f73381595a88235ec230011
MarilouF <farmer.marilou@hotmail.fr>
2020-06-19 14:43:03 -0400

    Add optional initial values for gpfit fonctions which didn't have them yet.

--- test/parameterestimation/maximumlikelihood/maximumlikelihood_gp_test.jl
+++ test/parameterestimation/maximumlikelihood/maximumlikelihood_gp_test.jl
@@ -27,6 +27,14 @@
 
     end
 
+    @testset "gpfit(y, initialvalues; logscalecov, shapecov)" begin
+        # model building with non-stationary logscale and shape
+        @test_logs Extremes.gpfit(y, zeros(Float64, 4),
+            logscalecov = [Variable("x₁", x₁)],
+            shapecov = [Variable("x₂", x₂)])
+
+    end
+
     @testset "gpfit(df, datacol; logscalecovid, shapecovid)" begin
         # model building with non-stationary location, logscale and shape
         df = DataFrame(y = y, x1 = x₁, x2 = x₂)
@@ -38,6 +46,14 @@
 
         @test fm.θ̂ .- var <= θ
         @test θ <= fm.θ̂ .+ var
+
+    end
+
+    @testset "gpfit(df, datacol, initialvalues; logscalecovid, shapecovid)" begin
+        # model building with non-stationary location, logscale and shape
+        df = DataFrame(y = y, x1 = x₁, x2 = x₂)
+
+        @test_logs Extremes.gpfit(df, :y, zeros(Float64, 4), logscalecovid = [:x1], shapecovid = [:x2])
 
     end
 
