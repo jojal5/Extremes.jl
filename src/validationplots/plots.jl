3a3616a22f01827b586ff32fbbc49ac73047a554
MarilouF <farmer.marilou@hotmail.fr>
2020-06-23 10:48:48 -0400

    Fix plots.

--- src/validationplots/plots.jl
+++ src/validationplots/plots.jl
@@ -1,25 +1,25 @@
 """
 # TODO : desc
 """
-function probplot(fm::Extremes.MaximumLikelihoodEVA) # TODO : Return value
+function probplot(fm::MaximumLikelihoodEVA)::Plot
 
-    y, p̂ = ecdf(fm.model.data)
+    y, p̂ = ecdf(fm.model.data.value)
 
     dist = Extremes.getdistribution(fm)[]
 
     p = cdf.(dist, y)
 
-    fig = plot(x=p, y=p̂, Geom.point, Geom.abline(color="red", style=:dash),
+    return plot(x=p, y=p̂, Geom.point, Geom.abline(color="red", style=:dash),
         Guide.xlabel("Model"), Guide.ylabel("Empirical"))
 
-end # TODO : Test
+end
 
 """
 # TODO : desc
 """
-function qqplot(fm::Extremes.MaximumLikelihoodEVA) # TODO : Return value
+function qqplot(fm::MaximumLikelihoodEVA)::Plot
 
-    y, p = ecdf(fm.model.data)
+    y, p = ecdf(fm.model.data.value)
 
     n = length(y)
 
@@ -29,17 +29,17 @@ function qqplot(fm::Extremes.MaximumLikelihoodEVA) # TODO : Return value
        q[i] = quantile(fm, p[i])[]
     end
 
-    fig = plot(x=q, y=y, Geom.point, Geom.abline(color="red", style=:dash),
+    return plot(x=q, y=y, Geom.point, Geom.abline(color="red", style=:dash),
         Guide.xlabel("Model"), Guide.ylabel("Empirical"))
 
-end # TODO : Test
+end
 
 """
 # TODO : desc
 """
-function returnlevelplot(fm::Extremes.MaximumLikelihoodEVA) # TODO : Return value
+function returnlevelplot(fm::MaximumLikelihoodEVA)::Plot
 
-    y, p = ecdf(fm.model.data)
+    y, p = ecdf(fm.model.data.value)
 
     T = 1 ./ (1 .- p)
 
@@ -56,18 +56,18 @@ function returnlevelplot(fm::Extremes.MaximumLikelihoodEVA) # TODO : Return valu
     l1 = layer(x=x, y=y, Geom.point)
     l2 = layer(x=x, y=q, Geom.line, Theme(default_color="red", line_style=[:dash]))
 
-    fig = plot(l1,l2, Scale.x_log10, Guide.xlabel("Return Period"), Guide.ylabel("Return Level"))
+    return plot(l1,l2, Scale.x_log10, Guide.xlabel("Return Period"), Guide.ylabel("Return Level"))
 
-end # TODO : Test
+end
 
 """
 # TODO : desc
 """
-function histplot(fm::Extremes.MaximumLikelihoodEVA) # TODO : Return value
+function histplot(fm::MaximumLikelihoodEVA)::Plot
 
     dist = Extremes.getdistribution(fm)[]
 
-    x = fm.model.data
+    x = fm.model.data.value
     n = length(x)
     nbin = Int64(ceil(sqrt(n)))
 
@@ -78,14 +78,14 @@ function histplot(fm::Extremes.MaximumLikelihoodEVA) # TODO : Return value
 
     h = layer(x=x, Geom.histogram(bincount=nbin, density=true))
     d = layer(x=xp, y=pdf.(dist, xp), Geom.line, Theme(default_color="red") )
-    plot(d,h, Coord.cartesian(xmin=xmin, xmax=xmax), Guide.xlabel("Data"), Guide.ylabel("Density"))
+    return plot(d,h, Coord.cartesian(xmin=xmin, xmax=xmax), Guide.xlabel("Data"), Guide.ylabel("Density"))
 
-end # TODO : Test
+end
 
 """
 # TODO : desc
 """
-function diagnosticplots(fm::Extremes.MaximumLikelihoodEVA) # TODO : Return value
+function diagnosticplots(fm::MaximumLikelihoodEVA)::Gadfly.Compose.Context
 
     f1 = probplot(fm)
     f2 = qqplot(fm)
@@ -93,4 +93,4 @@ function diagnosticplots(fm::Extremes.MaximumLikelihoodEVA) # TODO : Return valu
     f4 = histplot(fm)
 
     return gridstack([f1 f2; f3 f4])
-end # TODO : Test
+end
