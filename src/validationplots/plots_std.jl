3e6bcc3246485b2d40246c07fe9e5a86a5225e16
MarilouF <farmer.marilou@hotmail.fr>
2020-06-24 13:17:01 -0400

    Add std plots for ThresholdExceedance models

--- src/validationplots/plots_std.jl
+++ src/validationplots/plots_std.jl
@@ -1,16 +1,32 @@
 """
 # TODO : desc
 """
+function standardize(y::Real, μ::Real, σ::Real, ξ::Real)::Real
+    return 1 / ξ * log( 1 + ξ/σ * ( y - μ ) )
+
+end
+
+"""
+# TODO : desc
+"""
 function standardize(fm::MaximumLikelihoodEVA{BlockMaxima{T}} where T<:Distribution)::Vector{<:Real}
 
     y = fm.model.data.value
     d = Extremes.getdistribution(fm)
 
-    μ = location.(d)
-    σ = scale.(d)
-    ξ = shape.(d)
+    return standardize.(y, location.(d), scale.(d), shape.(d))
 
-    return 1 ./ ξ .* log.( 1 .+ ξ./σ .* ( y .- μ ) )
+end
+
+"""
+# TODO : desc
+"""
+function standardize(fm::MaximumLikelihoodEVA{ThresholdExceedance}, threshold::Real)::Vector{<:Real}
+
+    y = fm.model.data.value
+    d = Extremes.getdistribution(fm)
+
+    return standardize.(y, threshold, scale.(d), shape.(d))
 
 end
 
@@ -23,11 +39,30 @@ function probplot_std_data(fm::MaximumLikelihoodEVA{BlockMaxima{T}} where T<:Dis
 
     y, p̂ = ecdf(z)
 
-    dist = Gumbel(0,1)
+    return DataFrame(Model = cdf.(Gumbel(), y), Empirical = p̂)
 
-    p = cdf.(dist, y)
+end
 
-    return DataFrame(Model = p, Empirical = p̂)
+"""
+# TODO : desc
+"""
+function probplot_std_data(fm::MaximumLikelihoodEVA{ThresholdExceedance}, threshold::Real)::DataFrame
+
+    z = standardize(fm, threshold)
+
+    y, p̂ = ecdf(z)
+
+    return DataFrame(Model = cdf.(Gumbel(), y), Empirical = p̂)
+
+end
+
+"""
+# TODO : desc
+"""
+function probplot_std(df::DataFrame)::Plot
+
+    return plot(df, x=:Model, y=:Empirical, Geom.point, Geom.abline(color="red", style=:dash),
+        Guide.xlabel("Model"), Guide.ylabel("Empirical"), Guide.title("Probability Plot"), Guide.title("Residual Probability Plot"))
 
 end
 
@@ -38,8 +73,18 @@ function probplot_std(fm::MaximumLikelihoodEVA{BlockMaxima{T}} where T<:Distribu
 
     df = probplot_std_data(fm)
 
-    return plot(df, x=:Model, y=:Empirical, Geom.point, Geom.abline(color="red", style=:dash),
-        Guide.xlabel("Model"), Guide.ylabel("Empirical"), Guide.title("Probability Plot"), Guide.title("Residual Probability Plot"))
+    return probplot_std(df)
+
+end
+
+"""
+# TODO : desc
+"""
+function probplot_std(fm::MaximumLikelihoodEVA{ThresholdExceedance}, threshold::Real)::Plot
+
+    df = probplot_std_data(fm, threshold)
+
+    return probplot_std(df)
 
 end
 
@@ -52,9 +97,30 @@ function qqplot_std_data(fm::MaximumLikelihoodEVA{BlockMaxima{T}} where T<:Distr
 
     y, p = ecdf(z)
 
-    q = quantile.(Gumbel(0,1), p)
+    return DataFrame(Model = quantile.(Gumbel(), p), Empirical = y)
 
-    return DataFrame(Model = q, Empirical = y)
+end
+
+"""
+# TODO : desc
+"""
+function qqplot_std_data(fm::MaximumLikelihoodEVA{ThresholdExceedance}, threshold::Real)::DataFrame
+
+    z = standardize(fm, threshold)
+
+    y, p = ecdf(z)
+
+    return DataFrame(Model = quantile.(Gumbel(), p), Empirical = y)
+
+end
+
+"""
+# TODO : desc
+"""
+function qqplot_std(df::DataFrame)::Plot
+
+    return plot(df, x=:Model, y=:Empirical, Geom.point, Geom.abline(color="red", style=:dash),
+        Guide.xlabel("Model"), Guide.ylabel("Empirical"), Guide.title("Residual Quantile Plot"))
 
 end
 
@@ -65,8 +131,18 @@ function qqplot_std(fm::MaximumLikelihoodEVA{BlockMaxima{T}} where T<:Distributi
 
     df = qqplot_std_data(fm)
 
-    return plot(df, x=:Model, y=:Empirical, Geom.point, Geom.abline(color="red", style=:dash),
-        Guide.xlabel("Model"), Guide.ylabel("Empirical"), Guide.title("Residual Quantile Plot"))
+    return qqplot_std(df)
+
+end
+
+"""
+# TODO : desc
+"""
+function qqplot_std(fm::MaximumLikelihoodEVA{ThresholdExceedance}, threshold::Real)::Plot
+
+    df = qqplot_std_data(fm, threshold)
+
+    return qqplot_std(df)
 
 end
 
@@ -76,6 +152,17 @@ end
 function diagnosticplots_std(fm::MaximumLikelihoodEVA{BlockMaxima{T}} where T<:Distribution)::Gadfly.Compose.Context
     probplot = probplot_std(fm)
     qqplot = qqplot_std(fm)
+
+    return hstack(probplot, qqplot)
+
+end
+
+"""
+# TODO : Desc
+"""
+function diagnosticplots_std(fm::MaximumLikelihoodEVA{ThresholdExceedance}, threshold::Real)::Gadfly.Compose.Context
+    probplot = probplot_std(fm, threshold)
+    qqplot = qqplot_std(fm, threshold)
 
     return hstack(probplot, qqplot)
 
--- test/validationplots/plots_std_tests.jl
+++ test/validationplots/plots_std_tests.jl
@@ -1,38 +1,80 @@
 @testset "plots_std.jl" begin
     n = 5
     y = collect(1:n)
-    pd = GeneralizedExtremeValue(1.0, 1.0, 0.1)
+    threshold = 1.0
+    θ = [threshold, 1.0, 0.1]
+    pd = GeneralizedExtremeValue(θ...)
     std = [0, 0.9531, 1.8232, 2.6236, 3.3647]
 
-    fm = MaximumLikelihoodEVA(BlockMaxima(Variable("y", y)), [1.0, 0.0, 0.1])
+    fmbm = MaximumLikelihoodEVA(BlockMaxima(Variable("y", y)), [θ[1], log(θ[2]), θ[3]])
+    fmte = MaximumLikelihoodEVA(ThresholdExceedance(Variable("y", y)), [log(θ[2]), θ[3]])
+
+    @testset "standardize(y, μ, σ, ξ)" begin
+        # Simple standardized values
+        std̂ = Extremes.standardize(y[2], θ...)
+
+        @test std[2] ≈ std̂ atol = 0.0001
+
+    end
 
     @testset "standardize(fm)" begin
         # Simple standardized values
-        std̂ = Extremes.standardize(fm)
+        std̂ = Extremes.standardize(fmbm)
+
+        @test std ≈ std̂ atol = 0.0001
+
+    end
+
+    @testset "standardize(fm, threshold)" begin
+        # Simple standardized values
+        std̂ = Extremes.standardize(fmte, threshold)
 
         @test std ≈ std̂ atol = 0.0001
 
     end
 
     @testset "probplot_std_data(fm)" begin
-        df = probplot_std_data(fm)
+        df = probplot_std_data(fmbm)
 
         # Returns a dataframe with n values in column Model
         @test length(df[:, :Model]) == n
 
         # Returns a dataframe with n values in column Empirical
         @test length(df[:, :Empirical]) == n
+
+    end
+
+    @testset "probplot_std_data(fm, threshold)" begin
+        df = probplot_std_data(fmte, threshold)
+
+        # Returns a dataframe with n values in column Model
+        @test length(df[:, :Model]) == n
+
+        # Returns a dataframe with n values in column Empirical
+        @test length(df[:, :Empirical]) == n
+
+    end
+
+    @testset "probplot_std(df)" begin
+        # Plot does not throw
+        @test_logs Extremes.probplot_std(DataFrame(Model = collect(1:n), Empirical = collect(1:n)))
 
     end
 
     @testset "probplot_std(fm)" begin
         # Plot does not throw
-        @test_logs Extremes.probplot_std(fm)
+        @test_logs Extremes.probplot_std(fmbm)
+
+    end
+
+    @testset "probplot_std(fm, threshold)" begin
+        # Plot does not throw
+        @test_logs Extremes.probplot_std(fmte, threshold)
 
     end
 
     @testset "qqplot_std_data(fm)" begin
-        df = qqplot_std_data(fm)
+        df = qqplot_std_data(fmbm)
 
         # Returns a dataframe with n values in column Model
         @test length(df[:, :Model]) == n
@@ -42,15 +84,44 @@
 
     end
 
+    @testset "qqplot_std_data(fm, threshold)" begin
+        df = qqplot_std_data(fmte, threshold)
+
+        # Returns a dataframe with n values in column Model
+        @test length(df[:, :Model]) == n
+
+        # Returns a dataframe with n values in column Empirical
+        @test length(df[:, :Empirical]) == n
+
+    end
+
+    @testset "qqplot_std(df)" begin
+        # Plot does not throw
+        @test_logs Extremes.qqplot_std(DataFrame(Model = collect(1:n), Empirical = collect(1:n)))
+
+    end
+
     @testset "qqplot_std(fm)" begin
         # Plot does not throw
-        @test_logs Extremes.qqplot_std(fm)
+        @test_logs Extremes.qqplot_std(fmbm)
+
+    end
+
+    @testset "qqplot_std(fm, threshold)" begin
+        # Plot does not throw
+        @test_logs Extremes.qqplot_std(fmte, threshold)
 
     end
 
     @testset "diagnosticplots_std(fm)" begin
         # Plots do not throw
-        @test_logs Extremes.diagnosticplots_std(fm)
+        @test_logs Extremes.diagnosticplots_std(fmbm)
+
+    end
+
+    @testset "diagnosticplots_std(fm, threshold)" begin
+        # Plots do not throw
+        @test_logs Extremes.diagnosticplots_std(fmte, threshold)
 
     end
 
