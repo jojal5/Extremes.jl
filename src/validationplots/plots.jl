2bdaaef4b8f62e06416c3d23ee0553c380e51008
Jonathan Jalbert <jonathan.jalbert@polymtl.ca>
2020-07-01 15:48:34 -0400

    Add diagnostic plots for pwmEVA and BayesianEVA

--- src/validationplots/plots.jl
+++ src/validationplots/plots.jl
@@ -1,13 +1,25 @@
 """
 # TODO : desc
 """
-function probplot_data(fm::MaximumLikelihoodEVA)::DataFrame
+function probplot(fm::fittedEVA)::Plot
+
+    df = probplot_data(fm)
+
+    return plot(df, x=:Model, y=:Empirical, Geom.point, Geom.abline(color="red", style=:dash),
+        Guide.xlabel("Model"), Guide.ylabel("Empirical"), Guide.title("Probability Plot"))
+
+end
+
+"""
+# TODO : desc
+"""
+function probplot_data(fm::pwmEVA)::DataFrame
 
     checkstationarity(fm.model)
 
     y, p̂ = ecdf(fm.model.data.value)
 
-    dist = getdistribution(fm)[1]
+    dist = getdistribution(fm.model, fm.θ̂)[]
 
     p = cdf.(dist, y)
 
@@ -18,12 +30,79 @@ end
 """
 # TODO : desc
 """
-function probplot(fm::MaximumLikelihoodEVA)::Plot
+function probplot_data(fm::MaximumLikelihoodEVA)::DataFrame
 
-    df = probplot_data(fm)
+    checkstationarity(fm.model)
+
+    y, p̂ = ecdf(fm.model.data.value)
+
+    dist = getdistribution(fm.model, fm.θ̂)[]
+
+    p = cdf.(dist, y)
+
+    return DataFrame(Model = p, Empirical = p̂)
+
+end
+
+"""
+# TODO : desc
+"""
+function probplot_data(fm::BayesianEVA)::DataFrame
+
+    checkstationarity(fm.model)
+
+    y, p̂ = ecdf(fm.model.data.value)
+
+    dist = getdistribution(fm)
+
+    p_sim = Array{Float64}(undef, length(dist), length(y))
+
+    i = 1
+
+    for d in eachslice(dist, dims=1)
+        p_sim[i,:] = cdf.(d, y)
+        i +=1
+    end
+
+    p = vec(mean(p_sim, dims=1))
+
+    return DataFrame(Model = p, Empirical = p̂)
+
+end
+
+
+
+"""
+# TODO : desc
+"""
+function qqplot(fm::fittedEVA)::Plot
+
+    df = qqplot_data(fm)
 
     return plot(df, x=:Model, y=:Empirical, Geom.point, Geom.abline(color="red", style=:dash),
-        Guide.xlabel("Model"), Guide.ylabel("Empirical"), Guide.title("Probability Plot"))
+        Guide.xlabel("Model"), Guide.ylabel("Empirical"), Guide.title("Quantile Plot"))
+
+end
+
+
+"""
+# TODO : desc
+"""
+function qqplot_data(fm::pwmEVA)::DataFrame
+
+    checkstationarity(fm.model)
+
+    y, p = ecdf(fm.model.data.value)
+
+    n = length(y)
+
+    q = Vector{Float64}(undef, n)
+
+    for i in eachindex(p)
+       q[i] = quantile(fm, p[i])[1]
+    end
+
+    return DataFrame(Model = q, Empirical = y)
 
 end
 
@@ -51,19 +130,49 @@ end
 """
 # TODO : desc
 """
-function qqplot(fm::MaximumLikelihoodEVA)::Plot
+function qqplot_data(fm::BayesianEVA)::DataFrame
 
-    df = qqplot_data(fm)
+    checkstationarity(fm.model)
 
-    return plot(df, x=:Model, y=:Empirical, Geom.point, Geom.abline(color="red", style=:dash),
-        Guide.xlabel("Model"), Guide.ylabel("Empirical"), Guide.title("Quantile Plot"))
+    y, p = ecdf(fm.model.data.value)
+
+    dist = getdistribution(fm)
+
+    q_sim = Array{Float64}(undef, length(dist), length(y))
+
+    i = 1
+
+    for d in eachslice(dist, dims=1)
+        q_sim[i,:] = quantile.(d, p)
+        i +=1
+    end
+
+    q = vec(mean(q_sim, dims=1))
+
+    return DataFrame(Model = q, Empirical = y)
+
+end
+
+
+
+"""
+# TODO : desc
+"""
+function returnlevelplot(fm::fittedEVA)::Plot
+
+    df = returnlevelplot_data(fm)
+
+    l1 = layer(df, x=:Period, y=:Level,Geom.line, Theme(default_color="red", line_style=[:dash]))
+    l2 = layer(df, x=:Period, y=:Data, Geom.point)
+
+    return plot(l1,l2, Scale.x_log10, Guide.xlabel("Return Period"), Guide.ylabel("Return Level"), Guide.title("Return Level Plot"))
 
 end
 
 """
 # TODO : desc
 """
-function returnlevelplot_data(fm::MaximumLikelihoodEVA)::DataFrame
+function returnlevelplot_data(fm::pwmEVA)::DataFrame
 
     checkstationarity(fm.model)
 
@@ -86,16 +195,91 @@ end
 """
 # TODO : desc
 """
-function returnlevelplot(fm::MaximumLikelihoodEVA)::Plot
+function returnlevelplot_data(fm::MaximumLikelihoodEVA)::DataFrame
 
-    df = returnlevelplot_data(fm)
+    df = qqplot_data(fm)
 
-    l1 = layer(df, x=:Period, y=:Level,Geom.line, Theme(default_color="red", line_style=[:dash]))
-    l2 = layer(df, x=:Period, y=:Data, Geom.point)
+    checkstationarity(fm.model)
 
-    return plot(l1,l2, Scale.x_log10, Guide.xlabel("Return Period"), Guide.ylabel("Return Level"), Guide.title("Return Level Plot"))
+    y, p = ecdf(fm.model.data.value)
+
+    T = 1 ./ (1 .- p)
+
+    n = length(y)
+
+    q = Vector{Float64}(undef, n)
+
+    for i in eachindex(p)
+       q[i] = quantile(fm, p[i])[1]
+    end
+
+    return DataFrame(Data = y, Period = T, Level = q)
 
 end
+
+"""
+# TODO : desc
+"""
+function returnlevelplot_data(fm::BayesianEVA)::DataFrame
+
+    checkstationarity(fm.model)
+
+    y, p = ecdf(fm.model.data.value)
+
+    T = 1 ./ (1 .- p)
+
+    dist = getdistribution(fm)
+
+    q_sim = Array{Float64}(undef, length(dist), length(y))
+
+    i = 1
+    for d in eachslice(dist, dims=1)
+        q_sim[i,:] = quantile.(d, p)
+        i +=1
+    end
+
+    q = vec(mean(q_sim, dims=1))
+
+    return DataFrame(Data = y, Period = T, Level = q)
+
+end
+
+
+"""
+# TODO : desc
+"""
+function histplot(fm::fittedEVA)::Plot
+
+    dfs = histplot_data(fm)
+
+    h = layer(dfs[:h], x = :Data, Geom.histogram(bincount=dfs[:nbin], density=true))
+    d = layer(dfs[:d], x = :DataRange, y = :Density, Geom.line, Theme(default_color="red") )
+    return plot(d,h, Coord.cartesian(xmin = dfs[:xmin], xmax = dfs[:xmax]), Guide.xlabel("Data"), Guide.ylabel("Density"), Guide.title("Density Plot"))
+
+end
+
+"""
+# TODO : desc
+"""
+function histplot_data(fm::pwmEVA)::Dict
+
+    checkstationarity(fm.model)
+
+    dist = getdistribution(fm)[1]
+
+    x = fm.model.data.value
+    n = length(x)
+    nbin = Int64(ceil(sqrt(n)))
+
+    xmin = quantile(dist, 1/1000)
+    xmax = quantile(dist, 1 - 1/1000)
+    xp = range(xmin, xmax, length=1000)
+
+    return Dict(:h => DataFrame(Data = x), :d => DataFrame(DataRange = xp, Density = pdf.(dist, xp)),
+        :nbin => nbin, :xmin => xmin, :xmax => xmax)
+
+end
+
 
 """
 # TODO : desc
@@ -122,20 +306,39 @@ end
 """
 # TODO : desc
 """
-function histplot(fm::MaximumLikelihoodEVA)::Plot
+function histplot_data(fm::BayesianEVA)::Dict
 
-    dfs = histplot_data(fm)
+    checkstationarity(fm.model)
 
-    h = layer(dfs[:h], x = :Data, Geom.histogram(bincount=dfs[:nbin], density=true))
-    d = layer(dfs[:d], x = :DataRange, y = :Density, Geom.line, Theme(default_color="red") )
-    return plot(d,h, Coord.cartesian(xmin = dfs[:xmin], xmax = dfs[:xmax]), Guide.xlabel("Data"), Guide.ylabel("Density"), Guide.title("Density Plot"))
+    x = fm.model.data.value
+    n = length(x)
+    nbin = Int64(ceil(sqrt(n)))
+
+    dist = getdistribution(fm)
+
+    xmin = quantile(dist[1], 1/1000)
+    xmax = quantile(dist[1], 1 - 1/1000)
+    xp = range(xmin, xmax, length=1000)
+
+    h_sim = Array{Float64}(undef, length(dist), length(xp))
+
+    i = 1
+    for d in eachslice(dist, dims=1)
+        h_sim[i,:] = pdf.(d, xp)
+        i +=1
+    end
+
+    h = vec(mean(h_sim, dims=1))
+
+    return Dict(:h => DataFrame(Data = x), :d => DataFrame(DataRange = xp, Density = h),
+        :nbin => nbin, :xmin => xmin, :xmax => xmax)
 
 end
 
 """
 # TODO : desc
 """
-function diagnosticplots(fm::MaximumLikelihoodEVA)::Gadfly.Compose.Context
+function diagnosticplots(fm::fittedEVA)::Gadfly.Compose.Context
 
     f1 = probplot(fm)
     f2 = qqplot(fm)
@@ -146,8 +349,9 @@ function diagnosticplots(fm::MaximumLikelihoodEVA)::Gadfly.Compose.Context
 end
 
 
+
 """
-    mrl(y::Vector{<:Real}, steps::Int = 100)::DataFrame
+    mrlplot_data(y::Vector{<:Real}, steps::Int = 100)::DataFrame
 
 Compute the mean residual life from vector `y` using the set of thresholds from the `minimum(y)` value to the second-to-last larger value in `steps` steps.
 """
