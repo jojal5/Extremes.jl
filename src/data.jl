cb9c14fdaceb38864336795062cca23311115c34
Jonathan Jalbert <jonathan.jalbert@polymtl.ca>
2020-07-19 12:20:22 -0400

    Add docstring to dataset()

--- src/data.jl
+++ src/data.jl
@@ -1,8 +1,21 @@
 """
-    load(name::String)::DataFrame
+    dataset(name::String)::DataFrame
 
-Returns the data associated with the name.
+Load the dataset associated with `name`.
 
+Some datasets used by Coles (2001) are available using the following names:
+ - `portpirie`: annual maximum sea-levels in Port Pirie,
+ - `glass`: breaking strengths of glass fibers
+ - `fremantle`: annual maximum sea-levels in Fremantle
+ - `rain`: daily rainfall accumulations in south-west England
+ - `wooster`: daily minimum temperatures recorded in Wooster
+ - `dowjones`: daily closing prices of the Dow Jones Index
+ These datasets have been retrieved using the R package [*ismev*](https://cran.r-project.org/web/packages/ismev/index.html).
+
+# Examples
+```julia-repl
+julia> Extremes.dataset("portpirie")
+```
 """
 function dataset(name::String)::DataFrame
 
