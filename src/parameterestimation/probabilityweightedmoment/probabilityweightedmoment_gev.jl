7db2603e2eb75fb746596098a3093409e41ad13c
Jonathan Jalbert <jonathan.jalbert@polymtl.ca>
2020-07-19 23:55:41 -0400

    Fic the typos in gevfitpwm

--- src/parameterestimation/probabilityweightedmoment/probabilityweightedmoment_gev.jl
+++ src/parameterestimation/probabilityweightedmoment/probabilityweightedmoment_gev.jl
@@ -6,7 +6,7 @@ Estimate the GEV parameters with the probability weighted moments.
 
 # Implementation
 
-Estimation with the probability weighted moments, as described by [Hosking *et al. (1985)](https://www.tandfonline.com/doi/abs/10.1080/00401706.1985.10488049),
+Estimation with the probability weighted moments, as described by [Hosking *et al*. (1985)](https://www.tandfonline.com/doi/abs/10.1080/00401706.1985.10488049),
 is only possible in the stationary case.
 
 See also [`gevfitpwm`](@ref) for the other methods, [`gevfit`](@ref), [`gevfitbayes`](@ref) and [`BlockMaxima`](@ref).
