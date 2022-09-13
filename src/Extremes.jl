ad16b8c8febbc9ae91ca906427394ec8e06b7f87
Gabriel Gobeil <gabriel.gobeil199@gmail.com>
2022-09-13 08:52:27 -0400

    replace Mamba by AdvancedHMC and MCMCChains

--- src/Extremes.jl
+++ src/Extremes.jl
@@ -3,7 +3,7 @@ module Extremes
 using Distributions, DataFrames, Dates
 using Optim
 using LinearAlgebra, Statistics
-using Mamba, ProgressMeter
+using MCMCChains, AdvancedHMC
 using Gadfly
 
 import CSV
