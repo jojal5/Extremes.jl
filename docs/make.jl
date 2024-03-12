5d212c4d0f98079b93f36cf88244d40ef59e33af
jojal5 <Jonathan.jalbert@polymtl.ca>
2024-03-12 10:51:47 -0400

    Chenges to ThresholdExceedances doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,35 +1,51 @@
 using Documenter, Extremes
+import Random
 using Cairo, Fontconfig
 
-CI = get(ENV, "CI", nothing) == "true"
+# CI = get(ENV, "CI", nothing) == "true"
+
+# makedocs(sitename = "Extremes.jl",
+#     format = Documenter.HTML(
+#     prettyurls = CI,
+#     ),
+#     pages = [
+#        "index.md",
+#        "Tutorial" =>["Getting started" => "tutorial/index.md",
+#             "Block maxima" => "tutorial/BlockMaxima.md",
+#             "Threshold exceedance" => "tutorial/ThresholdExceedance.md",
+#             "Declustering" => "tutorial/Declustering.md",
+#             "Non-stationary block maxima" => "tutorial/blockmaxima_ns.md",
+#             "Non-stationary threshold exceedance" => "tutorial/thresholdexceedance_ns.md",
+#             "Additional features" => "tutorial/additional.md"],
+#        "contributing.md",
+#        "functions.md"
+#        ]
+
+# )
+
+# if CI
+#     deploydocs(
+#     repo   = "github.com/jojal5/Extremes.jl.git",
+#     devbranch = "dev",
+#     versions = ["stable" => "v^", "v#.#", "master"],
+#     push_preview = false,
+#     target = "build"
+#     )
+# end
+
+# deploydocs(repo = "github.com/jojal5/Extremes.jl.git")
+
 
 makedocs(sitename = "Extremes.jl",
     format = Documenter.HTML(
-    prettyurls = CI,
+    prettyurls = CI, size_threshold_ignore
     ),
     pages = [
-       "index.md",
-       "Tutorial" =>["Getting started" => "tutorial/index.md",
-            "Block maxima" => "tutorial/BlockMaxima.md",
+       "Tutorial" =>[
             "Threshold exceedance" => "tutorial/ThresholdExceedance.md",
             "Declustering" => "tutorial/Declustering.md",
             "Non-stationary block maxima" => "tutorial/blockmaxima_ns.md",
-            "Non-stationary threshold exceedance" => "tutorial/thresholdexceedance_ns.md",
-            "Additional features" => "tutorial/additional.md"],
-       "contributing.md",
-       "functions.md"
+            "Non-stationary threshold exceedance" => "tutorial/thresholdexceedance_ns.md"],
        ]
 
 )
-
-if CI
-    deploydocs(
-    repo   = "github.com/jojal5/Extremes.jl.git",
-    devbranch = "dev",
-    versions = ["stable" => "v^", "v#.#", "master"],
-    push_preview = false,
-    target = "build"
-    )
-end
-
-# deploydocs(repo = "github.com/jojal5/Extremes.jl.git")
