5cbf5ea27b119d1cddc6948629bbb813816eb29d
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2025-02-10 14:36:34 -0500

    fix typos

--- src/parameterestimation.jl
+++ src/parameterestimation.jl
@@ -1,4 +1,5 @@
 include(joinpath("parameterestimation", "bayesian.jl"))
 include(joinpath("parameterestimation", "maximumlikelihood.jl"))
 include(joinpath("parameterestimation", "probabilityweightedmoment.jl"))
+include(joinpath("parameterestimation", "lightweight_functions.jl"))
 
--- test/parameterestimation_test.jl
+++ test/parameterestimation_test.jl
@@ -3,5 +3,4 @@
     include(joinpath("parameterestimation", "maximumlikelihood_test.jl"))
     include(joinpath("parameterestimation", "probabilityweightedmoment_test.jl"))
     include(joinpath("parameterestimation", "lightweight_functions_test.jl"))
-
 end
